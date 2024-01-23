72cbd5acd582d3d08bcfc212ef113eb79afdb90e
Cristian Caro <crdcaro01@gmai.com>
2024-01-23 14:13:01 -0500

    agregar al carrito un producto

--- Front-End/masterpc-front/src/components/ItemDetail/ItemDetail.jsx
+++ Front-End/masterpc-front/src/components/ItemDetail/ItemDetail.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import Itemcount from '../ItemCount/Itemcount';
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import MisCompras from '../MisCompras/MisCompras';
 
 const ItemDetail = () => {
   const [product, setProduct] = useState(null);
-  const [isMounted, setIsMounted] = useState(true);
   const { productId } = useParams();
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(`http://localhost:3001/api/products/${productId}`);
-        if (response.ok && isMounted) {
+        if (response.ok) {
           const data = await response.json();
           setProduct(data);
         } else {
@@ -23,180 +23,88 @@ const ItemDetail = () => {
     };
 
     fetchData();
-
-    // Limpieza: establecer isMounted en false cuando el componente se desmonta
-    return () => setIsMounted(false);
-  }, [productId, isMounted]);
-
-  useEffect(() => {
-    // Manejar el cambio de productId aquí si es necesario
-    // Puedes realizar operaciones adicionales cuando el productId cambia
-    console.log('Nuevo productId:', productId);
   }, [productId]);
 
-  if (!product) {
-    return <div className='text-center color-blue-700 text-4xl'>Cargando...</div>;
-  }
+  useEffect(() => {
+    // Obtener el carrito actual del localStorage
+    const storedCart = JSON.parse(localStorage.getItem('cartProduct')) || [];
+    setCart(storedCart);
+  }, []);
+
+
+  const CartProduct = function (id, name, price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  };
+  
+
+  const handleAddToCart = () => {
+    const isProductInCart = cart.some(cartItem => cartItem.id === product._id);
+  
+    if (isProductInCart) {
+      const updatedCart = cart.map(cartItem =>
+        cartItem.id === product._id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      );
+      setCart(updatedCart);
+      localStorage.setItem('cartProduct', JSON.stringify(updatedCart));
+    } else {
+      const newCartItem = new CartProduct(product._id, product.name, product.price, 1);
+      setCart([...cart, newCartItem]);
+      localStorage.setItem('cartProduct', JSON.stringify([...cart, newCartItem]));
+    }
+  };
+
   return (
-    <div>
-
-    <h2 className='font-bold text-4xl md:text-5xl lg:text-6xl text-center my-8 mx-4 md:mx-20 lg:mx-20'>¡MASTERPC LOS MEJORES EN ESTO, CON EXCELENTES PRODUCTOS DE CALIDAD!</h2>
-   
-
-
- <div key={product.id} className="bg-white  shadow-lg  flex flex-col mx-auto mt-28 rounded-lg overflow-hidden hover:shadow-xl transition duration-300 transform hover:scale-105 w-1/2">
- <h3 className='text-2xl text-gray-800 font-semibold p-2'>Detalles del producto seleccionado:</h3>
-    <div className="px-6 py-4">
-      <h3 className="text-4xl font-semibold text-gray-800 mb-2 text-center">{product.name}</h3><br></br>
-      <p className="text-gray-600 text-2xl">{product.longDescription}</p>
-    </div>
-    <div className='flex justify-center'>
-      <img className=" w-75 h-96" src={product.mainPhoto} alt={product.name}/>
-
-        </div>
-    <div className="px-6 pt-2 pb-2">
-      <span className="text-sm font-semibold text-gray-700 text-2xl">Precio:</span>
-      <span className="text-xl font-bold text-gray-800">${product.price}</span>
-    </div>
-
-    <div className="px-6 pt-2 pb-4">
-      <span className="text-sm font-semibold text-gray-700 text-2xl">Stock:</span>
-      <span className="text-md text-gray-800 text-xl">{product.stock}</span>
-    </div>
-
-    <div className="px-6 pb-4">
-      <span className="text-sm font-semibold text-gray-700 text-2xl">Categoría:</span>
-      <span className="text-md text-gray-800 text-xl">{product.category}</span>
-    </div>
-
-    {/* Más detalles según sea necesario */}
-
-    <div className="px-6 pb-4">
-    <p className='p-5'><Itemcount/></p>
-     <div className='flex gap-5'>
-     <a
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
-        href="/"
-      >
-        Volver al Listado
-      </a>
-      <Link className='px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-8' to='/'>
-        Agregar al carrito
-      </Link>
-     </div>
-    </div>
-  </div>
-
-
-  </div>
-      )
-  }
-
-
-export  default ItemDetail
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom';
-
-// const ItemDetail = () => {
-//     // Desestructurar la información del producto
-//     const [product, setProduct] = useState(null);
-
-//     const {productId} =  useParams();
-//     console.log(productId)
-
-//     useEffect(()=>{
-//         const fecthData = async () =>{
-//             try{
-//                 const response = await fetch(`http://localhost:3001/api/products/${productId}`)
-//                 if(response.ok) {
-//                     const data = await response.json();
-//                     setProduct(data);
-                   
-//                 }
-//                 else{
-//                     console.error('Error al obtener el producto')
-//                 }
-//             }catch(error){
-//                 console.log(error)
-//             }
-//         }
-//         fecthData()
-//     },[productId])
-
-    
-//     // const { id, name, mainPhoto, longDescription, price, stock, category } = productId;
+    // Resto del código...
+       // Resto del código...
+       <div>
+       <h2 className='font-bold text-4xl md:text-5xl lg:text-6xl text-center my-8 mx-4 md:mx-20 lg:mx-20'>¡MASTERPC LOS MEJORES EN ESTO, CON EXCELENTES PRODUCTOS DE CALIDAD!</h2>
  
+       <div key={product?._id} className="bg-white  shadow-lg  flex flex-col mx-auto mt-28 rounded-lg overflow-hidden hover:shadow-xl transition duration-300 transform hover:scale-105 w-1/2">
+         <h3 className='text-2xl text-gray-800 font-semibold p-2'>Detalles del producto seleccionado:</h3>
+         <div className="px-6 py-4">
+           <h3 className="text-4xl font-semibold text-gray-800 mb-2 text-center">{product?.name}</h3><br></br>
+           <p className="text-gray-600 text-2xl">{product?.longDescription}</p>
+         </div>
+         <div className='flex justify-center'>
+           <img className="w-75 h-96" src={product?.mainPhoto} alt={product?.name} />
+         </div>
+         <div className="px-6 pt-2 pb-2">
+           <span className="text-sm font-semibold text-gray-700 text-2xl">Precio:</span>
+           <span className="text-xl font-bold text-gray-800">${product?.price}</span>
+         </div>
+         <div className="px-6 pt-2 pb-4">
+           <span className="text-sm font-semibold text-gray-700 text-2xl">Stock:</span>
+           <span className="text-md text-gray-800 text-xl">{product?.stock}</span>
+         </div>
+         <div className="px-6 pb-4">
+           <span className="text-sm font-semibold text-gray-700 text-2xl">Categoría:</span>
+           <span className="text-md text-gray-800 text-xl">{product?.category}</span>
+         </div>
+         {/* Renderizar Itemcount aquí según sea necesario */}
+         <div className="px-6 pb-4">
+           {/* <p className='p-5'><Itemcount /></p> */}
+           <div className='flex gap-5'>
+             <a
+               className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
+               href="/"
+             >
+               Volver al Listado
+             </a>
+             <button
+               className='px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-8'
+               onClick={() => handleAddToCart(product._id, product.name, product.price)}
+             >
+               Agregar al carrito
+             </button>
+           </div>
+         </div>
+       </div>
+     </div>
+  );
+};
 
-//   return (
-//     <div key={product.id}>
-//     <div className="max-w-xs mx-auto bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition duration-300 transform hover:scale-105">
-//       <img className="object-cover w-full h-48" src={product.mainPhoto} alt={product.name} />
-
-//       <div className="px-6 py-4">
-//         <h3 className="text-xl font-semibold text-gray-800">{product.name}</h3>
-//         <p className="mt-2 text-gray-600">{product.longDescription}</p>
-//       </div>
-
-//       <div className="px-6 pt-2 pb-2">
-//         <span className="text-sm font-semibold text-gray-700">Precio:</span>
-//         <span className="text-xl font-bold text-gray-800">${product.price}</span>
-//       </div>
-
-//       <div className="px-6 pt-2 pb-4">
-//         <span className="text-sm font-semibold text-gray-700">Stock:</span>
-//         <span className="text-md text-gray-800">{product.stock}</span>
-//       </div>
-
-//       <div className="px-6 pb-4">
-//         <span className="text-sm font-semibold text-gray-700">Categoría:</span>
-//         <span className="text-md text-gray-800">{product.category}</span>
-//       </div>
-
-//       {/* Aquí puedes agregar más detalles según sea necesario */}
-
-//       {/* Regresar al listado de productos */}
-//       <div className="px-6 pb-4">
-//         <a
-//           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
-//           href="/"
-//         >
-//           Volver al Listado
-//         </a>
-//       </div>
-//     </div>
-//   </div>
-//   )
-// }
-
-// export default ItemDetail
+export default ItemDetail;
--- Front-End/masterpc-front/src/components/MisCompras/MisCompras.jsx
+++ Front-End/masterpc-front/src/components/MisCompras/MisCompras.jsx
@@ -1,11 +1,115 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const MisCompras = () => {
-  return (
-    <div>
-      <h1>Bienvenido a tus compras</h1>
-    </div>
-  )
-}
 
-export default MisCompras
+  const [cartItem, setCartItem] = useState([]);
+
+  useEffect(() => {
+    // Obtener productos del localStorage
+    const storedCart = JSON.parse(localStorage.getItem('cartProduct')) || [];
+    setCartItem(storedCart);
+  }, []);
+
+  const handleRemoveFromCart = (productIdToRemove) => {
+    // Implementa la lógica para eliminar el producto del carrito.
+    // Puedes utilizar el estado local para actualizar el carrito después de eliminar.
+    const updatedCart = cartItem.filter(item => item.id !== productIdToRemove);
+    setCartItem(updatedCart);
+
+    // Actualizar el localStorage cuando cambie el carrito
+    localStorage.setItem('cartProduct', JSON.stringify(updatedCart));
+  };
+
+
+  
+  return (
+    <div className="container mx-auto my-8">
+      <h1 className="text-4xl font-bold mb-6">Bienvenido a tus compras</h1>
+
+      {/* Verifica si hay productos en el carrito */}
+      {cartItem.length ? (
+        <div className="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition duration-300 transform hover:scale-105">
+          {/* Muestra la información de los productos en el carrito */}
+          <table className="min-w-full bg-white border border-gray-300">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 py-2 px-4">Imagen</th>
+                <th className="border border-gray-300 py-2 px-4">Nombre</th>
+                <th className="border border-gray-300 py-2 px-4">Precio</th>
+                <th className="border border-gray-300 py-2 px-4">Categoría</th>
+                <th className="border border-gray-300 py-2 px-4">Eliminar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartItem.map(product => (
+                <tr key={product._id}>
+                  <td className="border border-gray-300 py-2 px-4">
+                    <img src={product.mainPhoto} alt={product.name} className="w-12 h-12 object-cover" />
+                  </td>
+                  <td className="border border-gray-300 py-2 px-4">{product.name}</td>
+                  <td className="border border-gray-300 py-2 px-4">${product.price}</td>
+                  <td className="border border-gray-300 py-2 px-4">{product.category}</td>
+                  <td className="border border-gray-300 py-2 px-4">
+                    <button
+                      className="bg-red-500 text-white rounded px-3 py-1"
+                      onClick={() => handleRemoveFromCart(product._id)}
+                    >
+                      Eliminar
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        // Mensaje si no hay productos en el carrito
+        <p>No hay productos en tu carrito.</p>
+      )}
+    </div>
+  );
+};
+
+export default MisCompras;
+
+
+
+
+
+
+
+  // const [products, setProducts] = useState([]);
+  // const [isMounted, setIsMounted] = useState(true);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch(`http://localhost:3001/api/products/${productId}`);
+  //       if (response.ok && isMounted) {
+  //         const data = await response.json();
+  //         setProducts([data]); // Almacena el producto como un array en el estado
+  //       } else {
+  //         console.error('Error en la respuesta:', response.status, response.statusText);
+  //       }
+  //     } catch (error) {
+  //       console.error('Error en la solicitud:', error.message);
+  //     }
+  //   };
+
+  //   fetchData();
+
+  //   // Limpieza: establecer isMounted en false cuando el componente se desmonta
+  //   return () => setIsMounted(false);
+  // }, [productId, isMounted]);
+
+  // if (!products.length && isMounted) {
+  //   return <div className='text-center color-blue-700 text-4xl'><i>Cargando...</i></div>;
+  // }
+
+  // const handleRemoveFromCart = (productIdToRemove) => {
+  //   // Implementa la lógica para eliminar el producto del carrito.
+  //   // Puedes utilizar el estado local para actualizar el carrito después de eliminar.
+  //   const updatedProducts = products.filter(product => product._id !== productIdToRemove);
+  //   setProducts(updatedProducts);
+  // };
