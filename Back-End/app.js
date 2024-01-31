25058ef2c26565fd8a3ee5c2922c7a5e198d5a22
Cristian Caro <99687046+Cristian20044121@users.noreply.github.com>
2024-01-30 22:05:34 -0500

    Update app.js

--- Back-End/app.js
+++ Back-End/app.js
@@ -7,7 +7,12 @@ const app = express();
 const PORT = config.PORT;
 
 // Configura CORS para permitir solicitudes desde cualquier origen (en desarrollo)
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://tudominio.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use(express.json());
 app.use('/api/products', RouterProducts);
